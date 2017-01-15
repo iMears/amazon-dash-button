c896b5ba60f779f9e9cb1a05696928d914c3cc6b
imears <maxwellmears@gmail.com>
2017-01-15 12:59:29 -0500

    Use lodash to throttle call to Apple because dash button will somtimes trigger multiple times for one press

--- dash.js
+++ dash.js
@@ -5,10 +5,13 @@ require('dotenv').config()
 const DashButton = require('node-dash-button');
 const dashButton = DashButton(process.env.DASH_ADDRESS, null, null, 'all');
 const findIPhone = require('./find');
+const _          = require('lodash');
 
-// warning this may trigger multiple times for one press
 console.log('Waiting for button push...')
+
 dashButton.on('detected', () => {
   console.log('Button push detected!');
-  findIPhone();
+
+  // stops trigger multiple times for one button push
+  _.throttle(findIPhone, 30000, { trailing: false });
 });
